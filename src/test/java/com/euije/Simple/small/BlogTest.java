da75818153fc659c012365e3ec8a136f6e1f2a39
이의제 <j@changtalk.kr>
2023-08-15 00:18:56 +0900

    테스트 코드 추가

---
+++ src/test/java/com/euije/Simple/small/BlogTest.java
@@ -0,0 +1,19 @@
+package com.euije.Simple.small;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BlogTest {
+
+    @Test
+    void GITHUB_ACTION을_위한_테스트에_성공합니다() {
+        // given
+
+        // when
+
+        // result
+        assertThat(1).isEqualTo(1);
+    }
+}
